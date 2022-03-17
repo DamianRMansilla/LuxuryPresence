eace620268452cdf62fceeb803ca7f78503f2a5a
DamianRMansilla <damianrmansilla85@gmail.com>
2022-03-17 04:45:06 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+let express = require("express");
+let path = require("path");
+let app = express();
 
 const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "\index.html"))
-})
 
 var puerto;
 if(process.env.PORT) {
-  puerto = process.env.PORT;
+    puerto = process.env.PORT;
 } else {
-  puerto = 3090;
+    puerto = 3090;
 }
 
 app.listen(puerto || puerto, function(){ console.log("Servidor corriendo en puerto 3090")})
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "\index.html"))
+})
 
 // app.listen(3090, ()=>{
 //     console.log("Servidor corriendo en el puerto 3090");
